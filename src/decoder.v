3006e866a9be477f62a56c5fc19dcf81859fbe4e
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-10-29 21:12:43 -0700

    Update decoder.v

--- src/decoder.v
+++ src/decoder.v
@@ -19,16 +19,23 @@ module seg7 (
     always @(*) begin
         case(counter)
             //                7654321
-            0:  segments = 7'b0111111;
-            1:  segments = 7'b0000110;
-            2:  segments = 7'b1011011;
-            3:  segments = 7'b1001111;
-            4:  segments = 7'b1100110;
-            5:  segments = 7'b1101101;
-            6:  segments = 7'b1111100;
-            7:  segments = 7'b0000111;
-            8:  segments = 7'b1111111;
-            9:  segments = 7'b1100111;
+            0: segments = 7'b0000000;
+            1: segments = 7'b0111110; //U
+            2: segments = 7'b1110111; //A
+            3: segments = 7'b1111100; //B
+            4: segments = 7'b0111001; //C
+            5: segments = 7'b1000000; //-
+            6: segments = 7'b1111001; //E
+            7: segments = 7'b0111000; //L 
+            8: segments = 7'b1111001; //E
+            9: segments = 7'b0111001; //C
+            10: segments = 7'b0110001; //T
+            11: segments = 7'b1010000; //R
+            12: segments = 7'b0111111; //O
+            13: segments = 7'b1010100; //N
+            14: segments = 7'b0110000; //I
+            15: segments = 7'b0111001; //C
+            16: segments = 7'b1110111; //A
             default:    
                 segments = 7'b0000000;
         endcase
